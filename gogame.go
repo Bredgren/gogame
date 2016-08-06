3e69ea6a606020de49ddaf756840f29b17d65767
Bredgren <brandon.edgren@gmail.com>
2016-08-05 21:41:24 -0700

    Choose default canvas.

---
+++ gogame.go
@@ -0,0 +1,46 @@
+// Package gogame is wrapper around gopherjs that makes it more convenient to work with for making games.
+// It also provides several utilities commonly used in games.
+package gogame
+
+import (
+	"log"
+
+	"github.com/gopherjs/gopherjs/js"
+	"github.com/gopherjs/jquery"
+)
+
+var jq = jquery.NewJQuery
+var console = js.Global.Get("console")
+
+var canvas *js.Object
+
+// Ready returns a channel that will send one item before closing, signaling that the
+// page has loaded and gogame is ready to be used.
+func Ready() chan struct{} {
+	ch := make(chan struct{}, 1)
+	jq("body").SetAttr("onload", func() {
+		canvas = jq("canvas").Get(0)
+		log.Println("gogame ready")
+		ch <- struct{}{}
+		close(ch)
+	})
+	return ch
+}
+
+// SetCanvas changes the canvas being used. Only one canvas may be used at a time. If
+// unset then gogame will default to the first canvas in the DOM.
+func SetCanvas(newCanvas *js.Object) {
+	canvas = newCanvas
+}
+
+// func SetMinCanvasSize() {
+//
+// }
+//
+// func SetMaxCanvasSize() {
+//
+// }
+//
+// func ToggleFullscreen() {
+//
+// }
