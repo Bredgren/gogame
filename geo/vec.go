0d29bc6127791749e6d46c45209f14e5e426a4d3
Bredgren <brandon.edgren@gmail.com>
2016-10-02 15:38:44 -0700

    Add 2D vector type.

---
+++ geo/vec.go
@@ -0,0 +1,71 @@
+package geo
+
+import "math"
+
+// Vec is a 2D vector.
+type Vec struct {
+	X, Y float64
+}
+
+// Len returns the length of the vector.
+func (v Vec) Len() float64 {
+	return math.Sqrt(v.X*v.X + v.Y*v.Y)
+}
+
+// Len2 is the length of the vector squared.
+func (v Vec) Len2() float64 {
+	return v.X*v.X + v.Y*v.Y
+}
+
+// SetLen sets the length of the vector.
+func (v *Vec) SetLen(l float64) {
+	len := math.Sqrt(v.X*v.X + v.Y*v.Y)
+	v.X = v.X / len * l
+	v.Y = v.Y / len * l
+}
+
+// Add modifies v to be the sum of v2 and itself.
+func (v *Vec) Add(v2 Vec) {
+	v.X += v2.X
+	v.Y += v2.Y
+}
+
+// Plus returns a new vector that is the sum of the two vectors.
+func (v Vec) Plus(v2 Vec) Vec {
+	return Vec{X: v.X + v2.X, Y: v.Y + v2.Y}
+}
+
+// Sub modifies v to be the difference between itself and v2.
+func (v *Vec) Sub(v2 Vec) {
+	v.X -= v2.X
+	v.Y -= v2.Y
+}
+
+// Minus returns a new vector that is the difference of the two vectors.
+func (v Vec) Minus(v2 Vec) Vec {
+	return Vec{X: v.X - v2.X, Y: v.Y - v2.Y}
+}
+
+// Mul modifies v to be itself times n.
+func (v *Vec) Mul(n float64) {
+	v.X *= n
+	v.Y *= n
+}
+
+// Times returns a new vector that is v times n.
+func (v Vec) Times(n float64) Vec {
+	return Vec{X: v.X * n, Y: v.Y * n}
+}
+
+// Normalize modifies v to be of length one in the same direction.
+func (v *Vec) Normalize() {
+	len := math.Sqrt(v.X*v.X + v.Y*v.Y)
+	v.X = v.X / len
+	v.Y = v.Y / len
+}
+
+// Normalized returns a new vector of length one in the same direction as v.
+func (v Vec) Normalized() Vec {
+	len := math.Sqrt(v.X*v.X + v.Y*v.Y)
+	return Vec{X: v.X / len, Y: v.Y / len}
+}
---
+++ geo/vec_test.go
@@ -0,0 +1,198 @@
+package geo
+
+import "testing"
+
+func TestVecLen(t *testing.T) {
+	cases := []struct {
+		v    Vec
+		want float64
+	}{
+		{Vec{X: 3, Y: 4}, 5},
+	}
+
+	for i, c := range cases {
+		got := c.v.Len()
+		if got != c.want {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecLen2(t *testing.T) {
+	cases := []struct {
+		v    Vec
+		want float64
+	}{
+		{Vec{X: 3, Y: 4}, 25},
+	}
+
+	for i, c := range cases {
+		got := c.v.Len2()
+		if got != c.want {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecSetLen(t *testing.T) {
+	cases := []struct {
+		v    Vec
+		want float64
+	}{
+		{Vec{X: 3, Y: 4}, 10},
+	}
+
+	for i, c := range cases {
+		c.v.SetLen(c.want)
+		got := c.v.Len()
+		if got != c.want {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecAdd(t *testing.T) {
+	cases := []struct {
+		v1   Vec
+		v2   Vec
+		want Vec
+	}{
+		{Vec{X: 3, Y: -4}, Vec{X: -3, Y: 4}, Vec{X: 0, Y: 0}},
+		{Vec{X: 3, Y: 4}, Vec{X: 3, Y: 4}, Vec{X: 6, Y: 8}},
+	}
+
+	for i, c := range cases {
+		got := c.v1
+		got.Add(c.v2)
+		if got != c.want {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecPlus(t *testing.T) {
+	cases := []struct {
+		v1   Vec
+		v2   Vec
+		want Vec
+	}{
+		{Vec{X: 3, Y: -4}, Vec{X: -3, Y: 4}, Vec{X: 0, Y: 0}},
+		{Vec{X: 3, Y: 4}, Vec{X: 3, Y: 4}, Vec{X: 6, Y: 8}},
+	}
+
+	for i, c := range cases {
+		got := c.v1.Plus(c.v2)
+		if got != c.want {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecSub(t *testing.T) {
+	cases := []struct {
+		v1   Vec
+		v2   Vec
+		want Vec
+	}{
+		{Vec{X: 3, Y: -4}, Vec{X: -3, Y: 4}, Vec{X: 6, Y: -8}},
+		{Vec{X: 3, Y: 4}, Vec{X: 3, Y: 4}, Vec{X: 0, Y: 0}},
+	}
+
+	for i, c := range cases {
+		got := c.v1
+		got.Sub(c.v2)
+		if got != c.want {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecMinus(t *testing.T) {
+	cases := []struct {
+		v1   Vec
+		v2   Vec
+		want Vec
+	}{
+		{Vec{X: 3, Y: -4}, Vec{X: -3, Y: 4}, Vec{X: 6, Y: -8}},
+		{Vec{X: 3, Y: 4}, Vec{X: 3, Y: 4}, Vec{X: 0, Y: 0}},
+	}
+
+	for i, c := range cases {
+		got := c.v1.Minus(c.v2)
+		if got != c.want {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecMul(t *testing.T) {
+	cases := []struct {
+		v    Vec
+		n    float64
+		want Vec
+	}{
+		{Vec{X: 3, Y: -4}, 2, Vec{X: 6, Y: -8}},
+		{Vec{X: 3, Y: 4}, 0, Vec{X: 0, Y: 0}},
+	}
+
+	for i, c := range cases {
+		got := c.v
+		got.Mul(c.n)
+		if got != c.want {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecTimes(t *testing.T) {
+	cases := []struct {
+		v    Vec
+		n    float64
+		want Vec
+	}{
+		{Vec{X: 3, Y: -4}, 2, Vec{X: 6, Y: -8}},
+		{Vec{X: 3, Y: 4}, 0, Vec{X: 0, Y: 0}},
+	}
+
+	for i, c := range cases {
+		got := c.v.Times(c.n)
+		if got != c.want {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecNormalize(t *testing.T) {
+	cases := []struct {
+		v    Vec
+		want Vec
+	}{
+		{Vec{X: 5, Y: 0}, Vec{X: 1, Y: 0}},
+		{Vec{X: 0, Y: -4}, Vec{X: 0, Y: -1}},
+	}
+
+	for i, c := range cases {
+		got := c.v
+		got.Normalize()
+		if got != c.want {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecNormalized(t *testing.T) {
+	cases := []struct {
+		v    Vec
+		want Vec
+	}{
+		{Vec{X: 5, Y: 0}, Vec{X: 1, Y: 0}},
+		{Vec{X: 0, Y: -4}, Vec{X: 0, Y: -1}},
+	}
+
+	for i, c := range cases {
+		got := c.v.Normalized()
+		if got != c.want {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
