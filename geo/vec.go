2449ef65525e168855ad5f3e9b83a02d364f0be1
Brandon Edgren <brandon.edgren@gmail.com>
2016-10-05 14:18:13 -0700

    Add limit and angle related functions to Vec.

--- geo/vec.go
+++ geo/vec.go
@@ -78,6 +78,42 @@ func (v Vec) Dot(v2 Vec) float64 {
 	return v.X*v2.X + v.Y*v2.Y
 }
 
+// Limit constrains the length of the vector be no greater than len. If the vector is already
+// less than len then no change is made.
+func (v *Vec) Limit(len float64) {
+	l := v.Len()
+	if l > len {
+		v.SetLen(len)
+	}
+}
+
+// Angle returns the radians relative to the positive x-axis (counterclockwise in screen
+// coordinates). The returned values is in the range (-π, π].
+func (v Vec) Angle() float64 {
+	return math.Atan2(v.Y, v.X)
+}
+
+// AngleFrom returns the radians from v2 to v (counterclockwise in screen coordinates).
+// The returned values is in the range (-π, π].
+func (v Vec) AngleFrom(v2 Vec) float64 {
+	r := math.Atan2(v.Y, v.X) - math.Atan2(v2.Y, v2.X)
+	if r > math.Pi {
+		r -= 2 * math.Pi
+	}
+	return r
+}
+
+// Rotate rotates the vector (counterclockwise in screen coordinates) by the given radians.
+func (v *Vec) Rotate(rad float64) {
+	v.X, v.Y = v.X*math.Cos(rad)-v.Y*math.Sin(rad), v.X*math.Sin(rad)+v.Y*math.Cos(rad)
+}
+
+// Rotated returns a new vector that is equal to this one rotated (counterclockwise in
+// screen coordinates) by the given radians.
+func (v Vec) Rotated(rad float64) Vec {
+	return Vec{X: v.X*math.Cos(rad) - v.Y*math.Sin(rad), Y: v.X*math.Sin(rad) + v.Y*math.Cos(rad)}
+}
+
 // RandVec returns a unit vector in a random direction.
 func RandVec() Vec {
 	rad := rand.Float64() * 2 * math.Pi
--- geo/vec_test.go
+++ geo/vec_test.go
@@ -220,11 +220,142 @@ func TestVecDot(t *testing.T) {
 
 func TestVecRand(t *testing.T) {
 	cases := 100000
-	maxErr := 1.0 / 100000000
+	maxErr := 1e-10
 	for i := 0; i < cases; i++ {
 		got := RandVec()
 		if math.Abs(got.Len()-1) > maxErr {
 			t.Errorf("case %d: %#v is length %f", i, got, got.Len())
 		}
 	}
+}
+
+func TestVecLimit(t *testing.T) {
+	cases := []struct {
+		v    Vec
+		len  float64
+		want Vec
+	}{
+		{Vec{X: 5, Y: 0}, 2, Vec{X: 2, Y: 0}},
+		{Vec{X: 0, Y: -4}, 2, Vec{X: 0, Y: -2}},
+		{Vec{X: 3, Y: 4}, 6, Vec{X: 3, Y: 4}},
+	}
+
+	for i, c := range cases {
+		got := c.v
+		got.Limit(c.len)
+		if got != c.want {
+			t.Errorf("case %d: len: %#v, got %#v, want %#v", i, c.len, got, c.want)
+		}
+	}
+}
+
+func TestVecAngle(t *testing.T) {
+	cases := []struct {
+		v    Vec
+		want float64
+	}{
+		{Vec{X: 5, Y: 0}, 0},
+		{Vec{X: -3, Y: 0}, math.Pi},
+		{Vec{X: 0, Y: -4}, -math.Pi / 2},
+		{Vec{X: 0, Y: 1}, math.Pi / 2},
+	}
+
+	for i, c := range cases {
+		got := c.v.Angle()
+		if math.Abs(got-c.want) > 1e-10 {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecAngleFrom(t *testing.T) {
+	cases := []struct {
+		v1, v2 Vec
+		want   float64
+	}{
+		{Vec{X: 5, Y: 0}, Vec{X: 2, Y: 0}, 0},
+		{Vec{X: -5, Y: 0}, Vec{X: -2, Y: 0}, 0},
+		{Vec{X: 0, Y: -4}, Vec{X: -1, Y: -1}, math.Pi / 4},
+		{Vec{X: -1, Y: 0}, Vec{X: 1, Y: 0}, math.Pi},
+		{Vec{X: -1, Y: 0}, Vec{X: 0, Y: -1}, -math.Pi / 2},
+	}
+
+	for i, c := range cases {
+		got := c.v1.AngleFrom(c.v2)
+		if math.Abs(got-c.want) > 1e-10 {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecRotate(t *testing.T) {
+	cases := []struct {
+		v    Vec
+		rad  float64
+		want Vec
+	}{
+		{Vec{X: 5, Y: 0}, 0, Vec{X: 5, Y: 0}},
+		{Vec{X: 3, Y: 0}, math.Pi / 2, Vec{X: 0, Y: 3}},
+		{Vec{X: 3, Y: 0}, -math.Pi / 2, Vec{X: 0, Y: -3}},
+		{Vec{X: 3, Y: 0}, math.Pi, Vec{X: -3, Y: 0}},
+		{Vec{X: 3, Y: 0}, -math.Pi, Vec{X: -3, Y: 0}},
+		{Vec{X: 0, Y: -1}, math.Pi, Vec{X: 0, Y: 1}},
+		{Vec{X: 0, Y: -1}, math.Pi / 4, Vec{X: 1, Y: -1}.Normalized()},
+		{Vec{X: -1, Y: 0}, -math.Pi, Vec{X: 1, Y: 0}},
+		{Vec{X: -1, Y: 0}, 3 * math.Pi / 2, Vec{X: 0, Y: 1}},
+	}
+
+	for i, c := range cases {
+		got := c.v
+		got.Rotate(c.rad)
+		if !vecsEqual(got, c.want) {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecRotated(t *testing.T) {
+	cases := []struct {
+		v    Vec
+		rad  float64
+		want Vec
+	}{
+		{Vec{X: 5, Y: 0}, 0, Vec{X: 5, Y: 0}},
+		{Vec{X: 3, Y: 0}, math.Pi / 2, Vec{X: 0, Y: 3}},
+		{Vec{X: 3, Y: 0}, -math.Pi / 2, Vec{X: 0, Y: -3}},
+		{Vec{X: 3, Y: 0}, math.Pi, Vec{X: -3, Y: 0}},
+		{Vec{X: 3, Y: 0}, -math.Pi, Vec{X: -3, Y: 0}},
+		{Vec{X: 0, Y: -1}, math.Pi, Vec{X: 0, Y: 1}},
+		{Vec{X: 0, Y: -1}, math.Pi / 4, Vec{X: 1, Y: -1}.Normalized()},
+		{Vec{X: -1, Y: 0}, -math.Pi, Vec{X: 1, Y: 0}},
+		{Vec{X: -1, Y: 0}, 3 * math.Pi / 2, Vec{X: 0, Y: 1}},
+	}
+
+	for i, c := range cases {
+		got := c.v.Rotated(c.rad)
+		if !vecsEqual(got, c.want) {
+			t.Errorf("case %d: got %#v, want %#v", i, got, c.want)
+		}
+	}
+}
+
+func TestVecRotateStress(t *testing.T) {
+	cases := 100000
+	for i := 0; i < cases; i++ {
+		v1 := RandVec()
+		v2 := RandVec()
+		between := v1.AngleFrom(v2)
+		rotated := v2.Rotated(between)
+		v2rotated := v2
+		v2rotated.Rotate(between)
+		if !vecsEqual(v1, rotated) || !vecsEqual(v1, v2rotated) {
+			t.Errorf("case %d: v1: %#v v2: %#v between: %#v rotated: %#v v2rotated: %#v",
+				i, v1, v2, between, rotated, v2rotated)
+		}
+	}
+}
+
+func vecsEqual(v1, v2 Vec) bool {
+	e := 1e-10
+	return math.Abs(v1.X-v2.X) < e && math.Abs(v1.Y-v2.Y) < e
 }
